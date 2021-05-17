40914b29cfb63b50cf45e541ee27247c842e8a3f
albenchris <albenchris00@gmail.com>
2021-05-17 16:14:01 -0500

    very basic AddItemForm component

--- client/src/components/AddItemForm/index.js
+++ client/src/components/AddItemForm/index.js
@@ -1,13 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/style.css';
 
 const AddItemForm = () => {
+    const [formState, setFormState] =
+        useState({
+            name: '',
+            link: '',
+            price: '',
+            specialNote: ''
+        });
+    const {
+        name,
+        link,
+        price,
+        specialNote
+    } = formState;
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(formState);
+    };
+
+    const handleChange = e => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
-        <div className="container">
-            <div>
-                AddItemForm says <span className="text-green">"hi!"</span>
-            </div>
-        </div>
+        <section>
+            <h2>Add an Item</h2>
+            <form
+                id='add-item-form'
+                onSubmit={handleSubmit}
+            >
+                <div className="form-input">
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        defaultValue={name}
+                        onBlur={handleChange}
+                    />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="link">Link:</label>
+                    <input
+                        type="text"
+                        name="link"
+                        defaultValue={link}
+                        onBlur={handleChange}
+                    />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="price">Price:</label>
+                    <input
+                        type="number"
+                        name="price"
+                        defaultValue={`$${price}`}
+                        onBlur={handleChange}
+                    />
+                </div>
+                <div className="form-input">
+                    <label htmlFor="specialNote">Special Notes:</label>
+                    <textarea
+                        name="specialNote"
+                        rows="5"
+                        defaultValue={specialNote}
+                        onBlur={handleChange}
+                    />
+                </div>
+
+                <button type="submit" data-testid="submit">
+                    Submit
+                </button>
+            </form>
+        </section>
     );
 };
 
