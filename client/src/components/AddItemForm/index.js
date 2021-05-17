1346a724eb84c3f408eaaa134c1b835318fa90f5
albenchris <albenchris00@gmail.com>
2021-05-17 15:51:00 -0500

    AddItemForm component renders in WishList component

---
+++ client/src/components/AddItemForm/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './css/style.css';
+
+const AddItemForm = () => {
+    return (
+        <div className="container">
+            <div>
+                AddItemForm says <span className="text-green">"hi!"</span>
+            </div>
+        </div>
+    );
+};
+
+export default AddItemForm;
--- client/src/components/WishList/index.js
+++ client/src/components/WishList/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './css/style.css';
 
+import AddItemForm from '../AddItemForm';
 import ListItem from '../ListItem';
 
 const WishList = () => {
@@ -11,6 +12,7 @@ const WishList = () => {
             <div>
                 WishList says <span className="text-purple">"Hello World!"</span>
             </div>
+            <AddItemForm />
             <ListItem />
         </div>
     );
