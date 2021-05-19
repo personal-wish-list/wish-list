b06587afb2beea68b41468188c4f9fb8e86d3b8d
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-05-18 21:45:57 -0500

    added typeDefs

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const Item = require('./Item');
 
 // if it's called WishList
 // const WishList = require('./WishList');
@@ -27,12 +28,17 @@ const userSchema = new Schema({
     required: true,
     minlength: 5
   },
-  // maybe this?
-  // wishList: [WishList.schema]
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  wishList: [Item.schema]
 });
 
 // set up pre-save middleware to create password
-userSchema.pre('save', async function(next) {
+userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
     this.password = await bcrypt.hash(this.password, saltRounds);
@@ -42,7 +48,7 @@ userSchema.pre('save', async function(next) {
 });
 
 // compare the incoming password with the hashed password
-userSchema.methods.isCorrectPassword = async function(password) {
+userSchema.methods.isCorrectPassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
 
