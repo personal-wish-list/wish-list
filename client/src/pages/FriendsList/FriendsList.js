847c2beb2a2963a641f9fb6df0dfd1fbfd8874a8
albenchris <albenchris00@gmail.com>
2021-05-26 11:08:07 -0500

    created FriendsList page to contain list of FriendCard components

---
+++ client/src/pages/FriendsList/FriendsList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './friends-list.css';
+
+import FriendCard from '../../components/FriendCard';
+
+const FriendsList = () => {
+    return (
+        <div className="container">
+
+
+            <FriendCard />
+
+        </div>
+    );
+};
+
+export default FriendsList;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import WishList from '../WishList/WishList';
 import SecretList from '../SecretList/SecretList';
 import ShoppingList from "../ShoppingList/ShoppingList";
 import AddWishListForm from "../../components/AddWishListForm/AddWishListForm";
+import FriendsList from "../FriendsList/FriendsList";
 
 const Home = () => {
     return (
@@ -12,8 +13,13 @@ const Home = () => {
 
             {/* =============== FOR TESTING PURPOSES ================= */}
             <AddWishListForm />
+            ======================================================================
             <WishList />
+            ======================================================================
+            <FriendsList />
+            ======================================================================
             <SecretList />
+            ======================================================================
             <ShoppingList />
             {/* ====================================================== */}
 
