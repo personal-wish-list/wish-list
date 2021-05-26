bff47b76039a7fe7970547d0c9f9b6666fce4d07
albenchris <albenchris00@gmail.com>
2021-05-26 15:04:17 -0500

    added a starting structure to Dashboard

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,30 @@
 import React from "react";
 import './dashboard.css';
+import { Link } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
 
 import WishList from '../WishList/WishList';
 
 const Dashboard = () => {
+    const wishlists = useSelector(state => state.wishlists);
+
+    if (!wishlists) return <div><AddWishListForm /></div>;
+
     return (
         <div className="container">
 
-
-            <WishList />
-
+            {wishlists && 
+                wishlists.map(wishlist => (
+                    <div>
+                        <Link
+                            to={`/wishlist/${wishlist._id}`}
+                        >
+                            {wishlist.name}
+                        </Link>
+                    </div>
+                ))
+            }
 
         </div>
     );
